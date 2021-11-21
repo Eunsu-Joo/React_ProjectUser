a42bf67be90cdeab53e71596fae81d945951212b
eunsu <holicholicpop@gmail.com>
2021-11-22 03:10:54 +0900

    🔹 Signup.js => 수정사항( validator,handleSubmit,onCheckUser) / TODO
    ◼️ 수정사항
    - useForm => validator 함수 생성
    => ❗ 사용안한이유 :  handleSubmit 커스텀불가.. , checkId 클릭을 못함.. form안에 data가 다 onSubmit으로 넘어감. 구글링해도 안나옴. API조작 불가..
    ✔️ validator 함수
    - 1. errors state 생성
    - 2. validator 안 errors 변수 만들어서 유효성에 어긋날때마다 errors에 담아줌( 애러 메세지 띄우기 위함.)
    - 3. formValid 변수생성 => 유효성 걸릴때마다 false로 만들어서 멈추게 해주는 역활
    - 4. setErrors에 validator안 errors 담아둠.
    - 5. return formValid
    ✔️ handleSubmit 함수
    - 1. validator true일떄 aysnc 로 데이터 보냄.
    - 2 성공 : 모달, 성공메세지 (onOpenModal) / 실패: 모달, 실패메세지 (onOpenModal / apiError state)
    ✔️ onCheckUser 함수
    - username 체크
    - 1. useContext로 data불러오기
    - 2. username만 배열로다시만들기 (map)
    - 3. value값 첫글자만 대문자로 바꾸기 (정규식 사용)
    - 4. includes로 결과 alert으로 띄우기
    - 5. useRef 생성 , checkId 버튼에 useRef 설정
    - 6. 아이디를 사용할 수 있다면 useRef를 false로 설정
    - 7. onSubmit에서 ref.current가 true일때 중복확인 버튼을 누르라는 alert 사용
    ◼️ TODO
    - 1. checkId 클릭 할 때 안에들어가는 값은 유효성 검사가 안됨.

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,26 +1,135 @@
-import { CheckIdBtn } from "components/Btn";
+import { CheckIdBtn, EnrollBtn } from "components/Btn";
 import styles from "./Signup.module.css";
-import { useForm } from "react-hook-form";
 import useModal from "hooks/useModal";
 import { Modal } from "portal/Modal";
-import { useState } from "react";
-import Validator from "common/Validator";
-const axios = require('axios').default;
+import { useEffect, useRef, useState } from "react";
 
+import { useContext } from "react/cjs/react.development";
+import { UserContext } from "context/UserContext"
+import { useInput } from "hooks/useInput";
+const axios = require('axios').default;
 export default () => {
-  console.log(Validator())
   const {open, onOpenModal,closeModal} = useModal(); 
-  const [error,setError]= useState(false);
-const {inputValidator:{name,username,email,website,phone},handleSubmit,reset,errors}= Validator('');
-  const onSubmit = async(data) =>{
-      await axios.post('https://jsonplaceholder.typicode.com/users',data)
-      .then(() => {onOpenModal(!open);reset();})
-    .catch(error =>{setError(true);onOpenModal(!open);console.log(error)})
-    
+  const [error,setError]= useState(null);
+  const [apiError,setApiError]= useState(null)
+  const [data,onChange]= useInput({
+    name:'',
+    username:'',
+    phone:'',
+    website:'',
+    email:''
+  })
+  const {name,username,email,phone,website}= data;
+  const {state:{data:users}}=useContext(UserContext);
+  const usernames= users.map(user => user.username);
+  const ref= useRef();
+  const onCheckUser=() => {
+    const value= data.username
+    const upperCaseValue = value.replace(/\b[a-z]/, letter => letter.toUpperCase());
+   if(usernames.includes(value)||usernames.includes(upperCaseValue)){
+     alert('다른아이디를 사용해주세요')
+   } else{
+ 
+     alert('사용할 수 있는 아이디입니다.')
+     ref.current= false;
+   }
   }
+  
+  const validator= () => {
+    let formValid= true;
+    let errors={};
+    //name
+    if(name === ''){
+      formValid = false;
+      errors["name"] = "Cannot be empty";
+    }
+    if(name !== ''){
+      if(!name.match(/^[가-힣]+$/)){
+        formValid = false;
+        errors["name"] = "Only Write Korean";
+        if(name.length>10){
+          formValid = false;
+          errors["name"] ="Less than 10 characters"
+        }
+      }
+    }
+    //username
+    if(username === ''){
+      formValid = false;
+      errors["username"] = "Cannot be empty";
+    }
+    if(username !== ''){
+      if(!username.match(/^[a-zA-Z0-9]*$/)){
+        formValid = false;
+        errors["username"] = "Input English & Number Form";
+        if(username.length>20){
+          formValid = false;
+          errors["username"] ="Less than 20 characters"
+        }
+      }
+    }
+    //phone
+    if(phone === ''){
+      formValid = false;
+      errors["phone"] = "Cannot be empty";
+    }
+    if(phone !== ''){
+      if(!phone.match(/^010-?([0-9]{3,4})-?([0-9]{4})$/)){
+        formValid = false;
+        errors["phone"] = "Input phone Form";
+        if(phone.length>11){
+          formValid = false;
+          errors["phone"] ="Less than 11 characters"
+        }
+      }
+    }
+    //email
+    if(email === ''){
+      formValid = false;
+      errors["email"] = "Cannot be empty";
+    }
+    if(email !== ''){
+      if(!email.match( /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i)){
+        formValid = false;
+        errors["email"] = "Input email form";
+        if(email.length>50){
+          formValid = false;
+          errors["email"] ="Less than 50 characters"
+        }
+      }
+    }
+    //website
+    if(website === ''){
+      formValid = false;
+      errors["website"] = "Cannot be empty";
+    }
+    if(website !== ''){
+      if(!website.match(  /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/)){
+        formValid = false;
+        errors["website"] = "Input website form";
+
+      }
+    }
+    setError({errors})
+    return formValid;
+  }
+  const handleSubmit= async(event) => {
+    event.preventDefault();
+    if(ref.current){
+     alert('중복확인을 해주세요');
+    }
+    if(validator()){
+      await axios.post('https://jsonplaceholder.typicode.com/users',data)
+      .then(() =>{onOpenModal(true)})
+      .catch(error =>{onOpenModal(true);setApiError(error);})
+    }
+  }
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
   return (
     <section className="container">
-      <form action="" autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+      <form action="" autoComplete="off" onSubmit={handleSubmit}>
         <div className={styles.imgBox}>
           <img
             src={process.env.PUBLIC_URL + `/images/user2.jpg`}
@@ -39,34 +148,38 @@ const {inputValidator:{name,username,email,website,phone},handleSubmit,reset,err
           </div>
           <h3>Enroll / Revise</h3>
           <div className={styles.inputBox}>
-            <input type="text" placeholder="Name" {...name} />{" "}
-            {errors.name && <p>{errors.name.message}</p>}
+            <input type="text" placeholder="Name" name="name" onChange={onChange}  />
+            {error !== null ? <p>{error.errors.name}</p> : null}
           </div>
           <div className={styles.inputBox}>
             <input
               type="text"
+              name="username"
               placeholder={"user Name"}
-              {...username}
+              onChange={onChange}
             />
-            <CheckIdBtn /> {errors.username && <p>{errors.username.message}</p>}
+                  <button className="btn" ref={ref} onClick={onCheckUser}>
+        CHECK ID
+      </button>
+      {error !== null ? <p>{error.errors.username}</p> : null}
           </div>
           <div className={styles.inputBox}>
-            <input type="text" placeholder="Phone" {...phone} />{" "}
-            {errors.phone && <p>{errors.phone.message}</p>}
+            <input type="text" placeholder="Phone" name="phone" onChange={onChange} />{" "}
+            {error !== null ? <p>{error.errors.phone}</p> : null}
           </div>{" "}
           <div className={styles.inputBox}>
-            <input type="text" placeholder="Email" {...email} />{" "}
-            {errors.email && <p>{errors.email.message}</p>}
+            <input type="text" placeholder="Email" name="email" onChange={onChange} />{" "}
+            {error !== null ? <p>{error.errors.email}</p> : null}
           </div>{" "}
           <div className={styles.inputBox}>
-            <input type="text" placeholder="Website" {...website} />{" "}
-            {errors.website && <p>{errors.website.message}</p>}
+            <input type="text" placeholder="Website" name="website" onChange={onChange} />{" "}
+            {error !== null ? <p>{error.errors.website}</p> : null}
           </div>
           <div className={styles.btns}>
-            <button className="btn">Enroll</button>
+            <EnrollBtn />
           </div>
         </article>
-        {open && <Modal onClose={closeModal}>{error? 'Find Error! Check your console' : 'Success Delete! Thank you'}</Modal> }
+        {open && <Modal onClose={closeModal}>{apiError ? 'Find Error! Check your console' : 'Success Create Account!'}</Modal> }
       </form>
     </section>
   );
