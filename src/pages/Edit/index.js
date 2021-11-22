5579bf58c56a774fc322f8b5e7a08ed87c4387b9
Eunsu-Joo <holicholicpop@gmail.com>
2021-11-22 11:10:07 +0900

    🔶 Edit.js => 수정사항 / ❓ 전체 데이터에서 id로  가져온이유 / ✨ 다른대안
    ◼ 수정사항
    ◾ common / Validtor.js 삭제
    => use-hook-form 사용안하므로 삭제함.
    ◾ validator 함수 추가
    - Signup.js 와 같은 validator함수 가져와서 적용.
    ◾ currentUser 가져와서 username placeholder에 적용
    - 1. context에서 데이터가져옴
    - 2. params로 id 가져옴.
    - 3. common에 만들어진 matchUser로 username가져오기.
    ❓ 전체 데이터에서 id로  가져온이유
    ◾.  edit.js의 경로는 꼭 UserDetail Page를 거쳐서 들어감. 그래서 UserDetail.js에서 데이터를 불러올때, context에 넣어서 사용해도됨.
    => 하지만 useFetch로 API를 불러왔고, Api를 가져오려면 useEffect를 사용해서 데이터를 가져와야 애러가안뜨는데, 그럼 useFetch를 고쳐야함.
    useFetch를 만든 목적과 어긋남.
    ◾ user의 length가 많지않아서 이렇게 가져옴...
    => IF. 이렇게 가져온다면 순서는? 1. userContext의 initialValue 에 username추가,  reducer type만들기 2.  context에 action함수 만들고, 파라미터로 username받기
    3.  UserDetail.js 페이지에서 useEffect로 user의 data받아오고, action함수 불러와서 dispatch함수, username넣기
    ✨ 다른대안
    ◾ user에 API 페이지 전환될때 다시 가져와서 username value얻기.
    ◾ UserDatil.js 데이터 불러올때 context state에 넣기

--- src/pages/Edit/index.js
+++ src/pages/Edit/index.js
@@ -1,95 +1,187 @@
-import {CheckIdBtn, ReviseBtn} from "components/Btn";
-import {useParams} from "react-router";
 import styles from "./Edit.module.css";
-import Validator from "common/Validator";
-import {useState} from "react";
+import { ReviseBtn } from "components/Btn";
+import { useParams } from "react-router";
+import { useInput } from "hooks/useInput";
+import { useContext, useState } from "react";
 import useModal from "hooks/useModal";
-import {useContext} from "react";
-import {UserContext} from "context/UserContext";
-import {matchUser} from "common/common";
-import {Modal} from "portal/Modal";
-const axios = require('axios').default;
-export default() => {
-    const {id} = useParams();
-    const {state: {
-            data
-        }} = useContext(UserContext);
-    const {username: currentUser} = matchUser(data, id);
-    const {open, onOpenModal, closeModal} = useModal();
-    const [error, setError] = useState(false);
-    const {
-        inputValidator: {
-            name,
-            username,
-            email,
-            website,
-            phone
-        },
-        handleSubmit,
-        reset,
-        errors
-    } = Validator(currentUser);
-    const onSubmit = async (data) => {
-        await axios
-            .put(`https://jsonplaceholder.typicode.com/users/${id}`, data)
-            .then(() => {
-                onOpenModal(!open);
-                reset();
-            })
-            .catch(error => {
-                setError(error);
-                onOpenModal(!open);
-            })
+import { matchUser } from "common/common";
+import { Modal } from "portal/Modal";
+import { UserContext } from "context/UserContext";
+const axios = require("axios").default;
+export default () => {
+  const [error, setError] = useState(null);
+  const [apiError, setApiError] = useState(null);
+  const { open, onOpenModal, closeModal } = useModal();
+  const {
+    state: { data: users },
+  } = useContext(UserContext);
+  const { id } = useParams();
+  const { username: currentUser } = matchUser(users, id);
+  const [data, onChange] = useInput({
+    name: "",
+    username: currentUser,
+    phone: "",
+    website: "",
+    email: "",
+  });
+  const { name, email, phone, website } = data;
+  const validator = () => {
+    let formValid = true;
+    let errors = {};
+    //name
+    if (name === "") {
+      formValid = false;
+      errors["name"] = "Cannot be empty";
+    }
+    if (name !== "") {
+      if (!name.match(/^[가-힣]+$/)) {
+        formValid = false;
+        errors["name"] = "Only Write Korean";
+        if (name.length > 10) {
+          formValid = false;
+          errors["name"] = "Less than 10 characters";
         }
-    return (
-        <section className="container">
-            <form action="" autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
-                <div className={styles.imgBox}>
-                    <img src={process.env.PUBLIC_URL + `/images/user${id}.jpg`} alt=""/>
-                </div>
-                <article className={styles.article}>
-                    <div className={styles.desc}>
-                        <h2>Contact</h2>
-                        <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                            Ipsum has been the industry's standard dummy text ever since the 1500s,
-                        </p>
-                        <span>You can subscribe us</span>
-                    </div>
-                    <h3>Enroll / Revise</h3>
-                    <div className={styles.inputBox}>
-                        <input type="text" placeholder="Name" {...name}/>{" "}
-                        {errors.name && <p>{errors.name.message}</p>}
-                    </div>
-                    <div className={styles.inputBox}>
-                        <input type="text" placeholder={currentUser} disabled={true} {...username}/>
-                        <button className="btn" disabled={true}>CHECK ID</button>
-                        {errors.username && <p>{errors.username.message}</p>}
-                    </div>
-                    <div className={styles.inputBox}>
-                        <input type="text" placeholder="Phone" {...phone}/>{" "}
-                        {errors.phone && <p>{errors.phone.message}</p>}
-                    </div>{" "}
-                    <div className={styles.inputBox}>
-                        <input type="text" placeholder="Email" {...email}/>{" "}
-                        {errors.email && <p>{errors.email.message}</p>}
-                    </div>{" "}
-                    <div className={styles.inputBox}>
-                        <input type="text" placeholder="Website" {...website}/>{" "}
-                        {errors.website && <p>{errors.website.message}</p>}
-                    </div>
-                    <div className={styles.btns}>
-                        <ReviseBtn />
-                    </div>
-                </article>
-            </form>
-            {
-                open && <Modal close={closeModal}>{
-                            error
-                                ? 'Find Error! Check your console'
-                                : 'Success Update Your Information!'
-                        }</Modal>
-            }
-        </section>
-    );
+      }
+    }
+    //phone
+    if (phone === "") {
+      formValid = false;
+      errors["phone"] = "Cannot be empty";
+    }
+    if (phone !== "") {
+      if (!phone.match(/^010-?([0-9]{3,4})-?([0-9]{4})$/)) {
+        formValid = false;
+        errors["phone"] = "Input phone Form";
+        if (phone.length > 11) {
+          formValid = false;
+          errors["phone"] = "Less than 11 characters";
+        }
+      }
+    }
+    //email
+    if (email === "") {
+      formValid = false;
+      errors["email"] = "Cannot be empty";
+    }
+    if (email !== "") {
+      if (!email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i)) {
+        formValid = false;
+        errors["email"] = "Input email form";
+        if (email.length > 50) {
+          formValid = false;
+          errors["email"] = "Less than 50 characters";
+        }
+      }
+    }
+    //website
+    if (website === "") {
+      formValid = false;
+      errors["website"] = "Cannot be empty";
+    }
+    if (website !== "") {
+      if (
+        !website.match(
+          /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+        )
+      ) {
+        formValid = false;
+        errors["website"] = "Input website form";
+      }
+    }
+    setError({ errors });
+    return formValid;
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (validator()) {
+      await axios
+        .put(`https://jsonplaceholder.typicode.com/users/${id}`, data)
+        .then(() => {
+          onOpenModal(true);
+        })
+        .catch((error) => {
+          onOpenModal(true);
+          setApiError(error);
+        });
+    }
+  };
+  return (
+    <section className="container">
+      <form action="" autoComplete="off" onSubmit={handleSubmit}>
+        <div className={styles.imgBox}>
+          <img src={process.env.PUBLIC_URL + `/images/user${id}.jpg`} alt="" />
+        </div>
+        <article className={styles.article}>
+          <div className={styles.desc}>
+            <h2>Contact</h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s,
+            </p>
+            <span>You can subscribe us</span>
+          </div>
+          <h3>Enroll / Revise</h3>
+          <div className={styles.inputBox}>
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              onChange={onChange}
+            />
+            {error !== null ? <p>{error.errors.name}</p> : null}
+          </div>
+          <div className={styles.inputBox}>
+            <input
+              type="text"
+              disabled={true}
+              name="username"
+              placeholder={currentUser}
+            />
+
+            <button className="btn" disabled={true}>
+              CHECK ID
+            </button>
+          </div>
+          <div className={styles.inputBox}>
+            <input
+              type="text"
+              placeholder="Phone"
+              name="phone"
+              onChange={onChange}
+            />
+            {error !== null ? <p>{error.errors.phone}</p> : null}
+          </div>{" "}
+          <div className={styles.inputBox}>
+            <input
+              type="text"
+              placeholder="Email"
+              name="email"
+              onChange={onChange}
+            />
+            {error !== null ? <p>{error.errors.email}</p> : null}
+          </div>{" "}
+          <div className={styles.inputBox}>
+            <input
+              type="text"
+              placeholder="Website"
+              name="website"
+              onChange={onChange}
+            />
+            {error !== null ? <p>{error.errors.website}</p> : null}
+          </div>
+          <div className={styles.btns}>
+            <ReviseBtn />
+          </div>
+        </article>
+      </form>
+      {open && (
+        <Modal onClose={closeModal}>
+          {apiError
+            ? "Find Error! Check your console"
+            : "Success Update Your Information!"}
+        </Modal>
+      )}
+    </section>
+  );
 };
