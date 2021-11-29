7afad229f47a639e8501cd740997e5a33d1b9845
Eunsu-Joo <holicholicpop@gmail.com>
2021-11-29 17:16:26 +0900

    ◽ components/ pagination.js 추가 => Home.js pagination 플로우
    ▪ 6개의 아이템 보여주는 페이지네이션 컴포넌트 추가
    -  Home.js
     1. Home.js => currentPage, pageNumber 담는 state만들기
     2. data slice로 짤라서 page 숫자만큼만 보여주는 paginate 함수 만들기 parameter(data,currentPage,pageNumber)
    => startIndex : currentPage-1 * pageNumber
    => endIndex : currentPage * pageNumber
    => return data.slice(startIndex,endIndex)
    => ex. currentPage=1 , pageNumber=6 (보여질 아이템 수)라면 ,
    시작인덱스 : 0, 마지막인덱스 :6 이 되고, slice는 마지막 인덱스 전만 짤라지므로 [0,1,2,3,4,5]로 짤라짐.
    currentPage=2 라면, 시작인덱스:6 , 마지막인덱스 :12 [6,7,8,9,10,11]로짤라짐.
    3. 짤라진 배열 변수에 담기.
    => const newArray = pagiante(data,pageNumber,currentPage);
    4. pagination에서 page 인자로 받아 현재 페이지 setter시키는 함수 만들어서 pagination에 보내기
    => 현재 페이지를 변화시켜, slice 로 짤라서 UI로 보여지는 것이므로 변화하는 setter함수가 필요함.
    => const handlePagination =(number) => setPage({...page:currentPage:number})
    5. pagination에 props로 보낼 변수, 함수 묶어서 props로 전달

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,12 +3,25 @@ import UserItem from "components/UserItem";
 import styles from "./Home.module.css";
 import useStore from "store/default";
 import shallow from "zustand/shallow";
+import Pagination from "components/Pagination";
 
 export default () => {
   const data = useStore((state) => state.data, shallow);
   const [input, setInput] = useState("");
-
-  const searchData = data.filter((user) => {
+  const [page, setPage] = useState({
+    pageNumber: 6,
+    currentPage: 1,
+  });
+  const { pageNumber, currentPage } = page;
+  const paginate = (data, currentPage, pageNumber) => {
+    const startIndex = (currentPage - 1) * pageNumber;
+    const endIndex = currentPage * pageNumber;
+    return data.slice(startIndex, endIndex);
+  };
+  const pageArray = paginate(data, currentPage, pageNumber);
+  const handlePagination = (num) => setPage({ ...page, currentPage: num });
+  const pages = { pageNumber, currentPage, handlePagination };
+  const searchData = pageArray.filter((user) => {
     const name = user.name.toLowerCase();
     const username = user.username.toLowerCase();
     if (input === "") {
@@ -47,6 +60,7 @@ export default () => {
               ))
             )}
           </div>
+          <Pagination {...pages} />
         </article>
       </section>
     </>
