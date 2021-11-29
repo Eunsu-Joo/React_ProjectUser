2145efd00f537c62a8fcdab174fcb22d688f9295
Eunsu-Joo <holicholicpop@gmail.com>
2021-11-29 15:20:20 +0900

    ▪ context 디렉토리, api.js ,Portal.js 삭제

--- src/portal/Portal.js
+++
@@ -1,8 +0,0 @@
-import { createPortal } from "react-dom";
-
-const Portal = ({ children }) => {
-  let modalRoot = document.getElementById("modal");
-
-  return createPortal(children, modalRoot);
-};
-export default Portal;
